2d8250c49d275d20351a538e4248a97603cc52b3
Alex Perathoner <alex.perathoner@gmail.com>
2020-02-05 16:14:16 +0100

    Working script. Also adding icon

--- RedirectPlease Extension/script.js
+++ RedirectPlease Extension/script.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     safari.extension.dispatchMessage("Hello World!");
 });
+
+(function() {
+    'use strict';
+
+    setTimeout(function() {
+        var path0 = window.location.pathname;
+        var path1 = path0.substring(0, 5);
+        if (path1 === "/u/0/") {
+            window.location.replace(path0.replace("/u/0/", "/u/1/"));
+        }
+    }, 100);
+ 
+	
+})();
